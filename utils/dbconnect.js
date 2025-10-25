9683f1456ee59c7f3c236606936123eb76d1dbf8
Ronit Sarkar <rseditz57@gmail.com>
2025-10-25 14:52:44 +0530

    v1

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -10,7 +10,7 @@ const mysql = require('mysql2')
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'adminuser',
-    password: 'admin12',
+    password: 'Vishal@13241',
     database: 'clydb'
 })
 
