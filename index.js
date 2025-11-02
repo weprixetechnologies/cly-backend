d5d6028d01483a6878142f79fed5988069ea7af8
Ronit Sarkar <rseditz57@gmail.com>
2025-11-02 08:51:34 +0530

    V

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ app.use(cors());
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 
 // Health check endpoint
