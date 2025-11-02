bd785fb72aea0653453596b51d338d3eef85aaeb
Ronit Sarkar <rseditz57@gmail.com>
2025-11-02 13:01:05 +0530

    m1

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(cors());
 
 
 
-app.use(express.json({ limit: '101mb' }));
-app.use(express.urlencoded({ limit: '101mb', extended: true }));
+app.use(express.json({ limit: '1000mb' }));
+app.use(express.urlencoded({ limit: '1000mb', extended: true }));
 
 
 
