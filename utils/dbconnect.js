64b73c1ce7ceb95f37de27fa2d33560fff10220d
Ronit Sarkar <rseditz57@gmail.com>
2026-07-11 03:11:50 +0530

    l

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2')
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'rseditz@222',
-    database: 'cly'
-})
-
 // const pool = mysql.createPool({
 //     host: '127.0.0.1',
-//     user: 'adminuser',
-//     password: 'Vishal@13241',
-//     database: 'clydb',
-//     timezone: '+05:30'
+//     user: 'root',
+//     password: 'rseditz@222',
+//     database: 'cly'
 // })
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'adminuser',
+    password: 'Vishal@13241',
+    database: 'clydb',
+    timezone: '+05:30'
+})
 
 
 module.exports = pool.promise();
